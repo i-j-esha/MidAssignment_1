a4e7fe9f1eae678abf9771d5bc71f58afb0c3b54
i-j-esha <70110741+i-j-esha@users.noreply.github.com>
2021-03-03 13:02:54 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,7 +22,19 @@ namespace MidAssignment_1
             ourBank.AddAccount(new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh")));
             ourBank.AddAccount(new Account(18101, "Alif", 3000, new Address("21", "52", "Dhaka", "Bangladdesh")));
             
-            
+            Account[] a = new Account[3];
+
+            a[0] = new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh\n"));
+            ourBank.AddAccount(a[0]);
+
+            a[0].Deposit(2000);
+            a[0].Withdraw(1500);
+
+            a[1] = new Account(18101, "Natasha", 10000, new Address("21", "52", "Dhaka", "Bangladdesh\n"));
+            ourBank.AddAccount(a[1]);
+
+            a[1].Deposit(5000);
+            a[1].Withdraw(3000);
             
             ourBank.PrintAccountDetails();
 
