05b449d3263a23823293931cf46c08fce5c9ccc7
i-j-esha <70110741+i-j-esha@users.noreply.github.com>
2021-03-03 13:06:03 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,25 +18,25 @@ namespace MidAssignment_1
             //Account account0 = new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh"));
             //account0.ShowAccountInformation();
 
-            Bank ourBank = new Bank("Developer Bank", 5);
-            ourBank.AddAccount(new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh")));
-            ourBank.AddAccount(new Account(18101, "Alif", 3000, new Address("21", "52", "Dhaka", "Bangladdesh")));
-            
+            Bank bank = new Bank("Developer Bank", 5);
+            bank.AddAccount(new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh\n")));
+            bank.AddAccount(new Account(18101, "Alif", 10000, new Address("21", "52", "Dhaka", "Bangladdesh\n")));
+
             Account[] a = new Account[3];
 
             a[0] = new Account(36160, "Bushra", 20000, new Address("05", "41", "Dhaka", "Bangladesh\n"));
-            ourBank.AddAccount(a[0]);
+            bank.AddAccount(a[0]);
 
             a[0].Deposit(2000);
             a[0].Withdraw(1500);
 
-            a[1] = new Account(18101, "Natasha", 10000, new Address("21", "52", "Dhaka", "Bangladdesh\n"));
-            ourBank.AddAccount(a[1]);
+            a[1] = new Account(18101, "Alif", 10000, new Address("21", "52", "Dhaka", "Bangladdesh\n"));
+            bank.AddAccount(a[1]);
 
             a[1].Deposit(5000);
             a[1].Withdraw(3000);
-            
-            ourBank.PrintAccountDetails();
+
+            bank.PrintAccountDetails();
 
 
         }
